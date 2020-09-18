ca6ba395f674d45bb93677817225683500586792
chemPolonium <li.boyuan@outlook.com>
2020-09-18 14:59:11 +0800

    finish quest 1

--- getTotalCg.m
+++ getTotalCg.m
@@ -8,17 +8,11 @@ function tankCg = getTankCg(tankQuantity,aircraftPitch,tankPosi,tankSize)
 tankCgOffset = getTankCgOffset(tankQuantity,aircraftPitch,tankSize);
 tankCg = tankPosi;
 tankCg([1 3],:) = tankCg([1 3],:) + tankCgOffset;
-cosPitch = cosd(aircraftPitch);
-sinPitch = sind(aircraftPitch);
-tankCg([1 3],:) = [cosPitch,-sinPitch;sinPitch,cosPitch] * tankCg([1 3],:);
 end
 
 function tankCgOffset = getTankCgOffset(tankQuantity,aircraftPitch,tankSize)
 tankBase = tankSize(1,:).*tankSize(2,:);
 oilCenterHeight = tankQuantity ./ tankBase;
-% oilFrontHeight = oilCenterHeight-tankSize(:,1)*cosd(aircraftPitch)/2;
-% oilRearHeight = oilCenterHeight+tankSize(:,1)*cosd(aircraftPitch)/2;
-% tankCgX = tankSize(:,1)*(2*oilFrontHeight+oilRearHeight)/12/oilCenterHeight;
 tankCgX = tankSize(1,:).^2 * tand(aircraftPitch) ./ (12 * oilCenterHeight);
 tankCgZ = (oilCenterHeight - tankSize(3,:)) / 2;
 tankCgOffset = [tankCgX;tankCgZ];
--- quest1.m
+++ quest1.m
@@ -5,7 +5,7 @@ load('quest1.mat');
 totalCg = zeros(size(t,1),3);
 tankQuantity = tankInitQuantity - cumsum(tankFlow/oilDensity);
 for i = 1:numel(t)
-    totalCg(i,:) = getTotalCg(tankQuantity(i),...
+    totalCg(i,:) = getTotalCg(tankQuantity(i,:),...
         aircraftPitch(i),tankPosi,tankSize,aircraftMass,oilDensity)';
 end
 
