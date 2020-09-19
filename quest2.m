979685349a4c4967ea9fa49e5ebe38439ce6e289
chemPolonium <li.boyuan@outlook.com>
2020-09-19 21:33:18 +0800

    more work on quest 2

--- quest2.m
+++ quest2.m
@@ -2,34 +2,51 @@ clear,clc;
 load('tankData.mat');
 load('quest2.mat');
 
-actTank = [2 3 4 5];
+iActTank = logical([0 1 1 0 0 0]);
+engineTank = logical([0 1 1 1 1 0]);
 % tankPosiGain = -tankPosi;
 % tankPosiGain(:,[1 6]) = tankPosiGain(:,[1 6]) - tankPosiGain(:,[2 5]);
 % 
 % actTankPosiGain = tankPosiGain(actTank);
-actTankMaxFlow = tankMaxFlow(actTank);
+% actTankMaxFlow = tankMaxFlow(actTank);
 
 [tankFlow,tankQuantity] = deal(zeros(size(t,1),6));
+actTank = false(size(t,1),6);
 tankQuantity(1,:) = tankInitQuantity;
+actTank(1,:) = iActTank;
+
+actTankStateflow = quest2chart('actTank',iActTank,'tankQuantity',tankInitQuantity);
 
 totalCg = zeros(size(t,1),3);
 totalCg(1,:) = getTotalCg(tankQuantity(1,:),0,tankPosi,tankSize,aircraftMass,oilDensity)';
 
 for i = 1:numel(t)-1
-    problem.objective = @(x)norm(getTotalCgFromFlow(tankQuantity(i,:),actTank,x,0,tankPosi,tankSize,aircraftMass,oilDensity)');
-    problem.x0 = ones(size(actTank))/6*aircraftFlow(i);
-    problem.lb = zeros(size(actTank))';
-    problem.ub = actTankMaxFlow';
+    problem.objective = @(x)norm(getTotalCgFromFlow(...
+        tankQuantity(i,:),...
+        iActTank,...
+        x,...
+        0,...
+        tankPosi,...
+        tankSize,...
+        aircraftMass,...
+        oilDensity)');
+    problem.x0 = ones(sum(iActTank),1)/6*aircraftFlow(i);
+    problem.lb = zeros(sum(iActTank),1);
+    problem.ub = tankMaxFlow(iActTank);
     problem.Aineq = [];
     problem.bineq = [];
-    problem.Aeq = ones(size(actTank));
+    actEngineTank = iActTank & engineTank;
+    problem.Aeq = double(actEngineTank(iActTank));
     problem.Beq = aircraftFlow(i);
     problem.nonlcon = [];
     problem.solver = 'fmincon';
     problem.options = optimoptions('fmincon','Display','none');
-    tankFlow(i,:) = fmincon(problem);
+    tankFlow(i,iActTank) = fmincon(problem)';
     tankQuantity(i+1,:) = tankQuantity(i,:);
-    tankQuantity(i+1,actTank) = tankQuantity(i+1,actTank) - tankFlow(i,:)/oilDensity;
+    tankQuantity(i+1,:) = tankQuantity(i+1,:) - tankFlow(i,:)/oilDensity;
+    step(actTankStateflow,'tankQuantity',tankQuantity(i+1,:));
+    iActTank = actTankStateflow.actTank;
+    actTank(i+1,:) = iActTank;
     disp(i);
 end
 
